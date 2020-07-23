a805b9c1aae087b7f78b01f8e3d0e08567463ad9
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-23 16:45:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require 'pry'
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+   @@count = 0 
+   @@artists = []
+   @@genres = []
+   @@artist_count = {} 
+   @@genre_count = 0 
+   @@genre_count = {}
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@artists << @artist 
+    @@genres << @genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+   def self.artists
+    @@artists.uniq 
+  end
+  
+  def self.genres 
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genre_count << @genre
+  end
+  
+  
+  
+  
+  
+  
+  
+end
