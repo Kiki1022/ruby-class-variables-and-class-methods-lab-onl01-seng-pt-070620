13d0dc0e0a3443e5ea0e2af3683a20f395025621
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-23 17:12:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   def self.genre_count
      @@genre_count = {}
      
-     @@genre_count.each do|genre, genre.count|
+     @@genre_count.each do|genre, genre.total|
    end
   end
   
